1b125c7e784576d18a9931ba02fb5890a9cc71f6
ivan ibarra <kamisilver11@gmail.com>
2023-07-27 12:34:21 -0700

    done with sales

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,6 +3,12 @@ import MainPage from './MainPage';
 import Nav from './Nav';
 import ManufacturersList from './ManufacturersList';
 import ManufacturerForm from './ManufacturerForm';
+import CustomerForm from './Sales/CustomerForm';
+import RecordSaleForm from './Sales/RecordSaleForm';
+import ListSalesPeople from './Sales/ListSalesPeople';
+import ListAllSales from './Sales/ListAllSales';
+import ListCustomers from './Sales/ListCustomers';
+import SalesPersonForm from './Sales/SalesPersonForm';
 
 function App() {
   return (
@@ -11,6 +17,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="customers/list/" element={<ListCustomers />} />
+          <Route path="customers/" element={<CustomerForm />} />
+          <Route path="salespeople/list/" element={<ListSalesPeople />} />
+          <Route path="salespeople/" element={<SalesPersonForm />} />
+          <Route path="sales/" element={<RecordSaleForm />} />
+          <Route path="sales/history/" element={<ListAllSales />} />
           <Route path="manufacturers">
             <Route index element={<ManufacturersList />} />
             <Route path="create" element={<ManufacturerForm/>} />
